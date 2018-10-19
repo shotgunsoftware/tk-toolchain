b395e41a2667144b7109eeb4ae4de0ee48bc57f7
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2018-10-19 15:17:45 -0400

    now works with other repos

--- pytest_tk_build.py
+++ pytest_tk_build.py
@@ -21,8 +21,9 @@ def _find_repo_root(path):
 
 
 def _update_sys_path(reason, path):
-    print("{0}: {1}".format(reason, path))
-    sys.path.insert(0, path)
+    if os.path.exists(path):
+        print("{0}: {1}".format(reason, path))
+        sys.path.insert(0, path)
 
 
 def _initialize_logging(config):
@@ -43,12 +44,22 @@ def pytest_configure(config):
 
     repo_root = _find_repo_root(cur_dir)
 
+    # If we're not in the tk-core repo, we should add the current repo's python folder since it may
+    # have custom tools.
     if os.path.basename(repo_root).lower() != "tk-core":
         _update_sys_path(
             "Adding repository tests/python folder", os.path.join(repo_root, "tests", "python")
         )
+        _update_sys_path(
+            "Adding repository tests/python folder", os.path.join(repo_root, "tests", "python", "third_party")
+        )
 
-    tk_core_repo_root = os.path.join(os.path.dirname(repo_root), "tk-core")
+    repos_root = os.path.dirname(repo_root)
+    # FIME: Named this way to please the publisher tests, but should probably
+    # be named SHOTGUN_REPOS_ROOT
+    os.environ["SHOTGUN_EXTERNAL_REPOS_ROOT"] = repos_root
+
+    tk_core_repo_root = os.path.join(repos_root, "tk-core")
 
     _update_sys_path(
         "Adding Toolkit folder", os.path.join(tk_core_repo_root, "python")
@@ -69,4 +80,7 @@ def pytest_configure(config):
 
 # Ignore unit tests found inside tk-core.
 def pytest_ignore_collect(path, config):
-    return os.path.join("tests", "python", "third_party") in str(path)
+    return (
+        os.path.join("tests", "python", "third_party") in str(path) or
+        os.path.join("tests", "fixtures") in str(path)
+    )
