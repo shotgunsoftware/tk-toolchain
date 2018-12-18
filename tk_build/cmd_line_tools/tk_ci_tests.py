227b364a60594d6de6448635bd68e964103a7703
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2018-12-18 16:48:27 -0500

    run extra commands from tk-build.yml file

--- tk_build/cmd_line_tools/tk_ci_tests.py
+++ tk_build/cmd_line_tools/tk_ci_tests.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
 import os
 import subprocess
+import yaml
 
-from tk_build import repo
+from tk_build import repo, ci
 from tk_build.cmd_line_tools import tk_docs
 
 
@@ -14,15 +15,37 @@ def _print_header(text):
 
 def main():
 
+    if ci.is_travis():
+        suffix = os.path.expandvars("travis_${TRAVIS_EVENT_TYPE}_${TRAVIS_PYTHON_VERSION}")
+    else:
+        # TODO: Our builds do not run in parallel on appveyor so keep simple for now.
+        suffix = "app_veyor"
+
+    env = {}
+    env.update(os.environ)
+    env["SHOTGUN_TEST_ENTITY_SUFFIX"] = suffix
+
     _print_header("Running tests...")
 
-    subprocess.check_call(["pytest", "--cov"])
+    subprocess.check_call(["pytest", "--cov"], env=env)
+
+    repo_root = repo.find_repo_root()
 
     # If there is documentation, run it.
-    if os.path.exists(os.path.join(repo.find_repo_root(), "docs")):
+    if os.path.exists(os.path.join(repo_root, "docs")):
         print()
         _print_header("Generating documentation...")
         tk_docs.main()
+
+    # Run any extra build comments from the .tx-build.yml file.
+    tk_build_path = os.path.join(repo_root, ".tk-build.yml")
+    if os.path.exists(tk_build_path):
+        with open(tk_build_path, "rt") as fh:
+            data = yaml.safe_load(fh)
+
+        for command in data.get("script") or []:
+            print(command)
+            subprocess.check_call(command, shell=True, env=env)
 
 
 if __name__ == "__main__":
