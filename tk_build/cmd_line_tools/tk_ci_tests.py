8c0ed79a593dd91058a46a1d3cb2d5692d698c99
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2018-12-17 21:58:44 -0500

    coverage

--- tk_build/cmd_line_tools/tk_ci_tests.py
+++ tk_build/cmd_line_tools/tk_ci_tests.py
@@ -1,20 +1,13 @@
 import os
-import warnings
-
-import pytest
-from pytest import PytestWarning
+import subprocess
 
 from tk_build import ci
-
 from tk_build.cmd_line_tools import tk_docs
 
 
 def main():
 
-    if os.path.basename(ci.get_cloned_folder_root()) != "tk-build":
-        pytest.main(["pytest", "--cov"])
-    else: # If the repo we're running in the CI is us, we'll append out coverage instead.
-        pytest.main()
+    subprocess.check_call(["pytest", "--cov"])
 
     # If there is documentation, run it.
     if os.path.exists(os.path.join(ci.get_cloned_folder_root(), "docs")):
