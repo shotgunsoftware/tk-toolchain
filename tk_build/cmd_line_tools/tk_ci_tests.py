04d57dd7d21a3fb3d4629fa5c6f828a0cc62969d
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2018-12-18 14:57:04 -0500

    separators for each part of the tk-ci-tests

--- tk_build/cmd_line_tools/tk_ci_tests.py
+++ tk_build/cmd_line_tools/tk_ci_tests.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import os
 import subprocess
 
@@ -5,12 +6,22 @@ from tk_build import repo
 from tk_build.cmd_line_tools import tk_docs
 
 
+def _print_header(text):
+    print("=" * 79)
+    print(text)
+    print("=" * 79)
+
+
 def main():
+
+    _print_header("Running tests...")
 
     subprocess.check_call(["pytest", "--cov"])
 
     # If there is documentation, run it.
     if os.path.exists(os.path.join(repo.find_repo_root(), "docs")):
+        print()
+        _print_header("Generating documentation...")
         tk_docs.main()
 
 
