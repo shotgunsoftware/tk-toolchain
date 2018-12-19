6a7d1f48c3446fb9632681bcd42b422f9c8d4ede
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2018-12-18 20:58:22 -0500

    fixing dry run forcing

--- tk_build/cmd_line_tools/tk_ci_install.py
+++ tk_build/cmd_line_tools/tk_ci_install.py
@@ -142,7 +142,7 @@ def main():
 
     is_dry_run = arguments["--dry-run"]
 
-    if ci.is_in_ci_environment():
+    if not ci.is_in_ci_environment():
         print("Forcing --dry-run since this is not run from a ci environment.")
         is_dry_run = True
 
