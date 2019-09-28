0e3067ed636c92fe22c0e438d01b76b5e1ffa6e6
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2019-09-27 22:30:13 -0400

    added missing file

---
+++ tk_toolchain/util.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+import os
+
+
+def expand_path(path):
+    """
+    Expand a path's environment variables and ``~``.
+
+    :param str path: Path to expand.
+
+    :returns: The expanded path.
+    """
+    # TODO: What is an env var contains an env var?
+    return os.path.expanduser(os.path.expandvars(path))
