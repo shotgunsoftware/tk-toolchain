ccb622b7cb27326d7470aad34096f8a9d0c57503
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2019-10-30 10:12:37 -0400

    fixed some comments

--- tests/test_tk_docs_preview.py
+++ tests/test_tk_docs_preview.py
@@ -23,15 +23,19 @@ TK_FRAMEWORK_ROOT = os.path.join(REPOS_ROOT, "tk-framework-shotgunutils")
 PYTHON_API_ROOT = os.path.join(REPOS_ROOT, "python-api")
 
 
-# Note: These tests are likely to introduce side effects because they monkey patch toolkit.
-# For now we're running the in-process because it makes coverage more easier to retrieve
+# Note: These tests are likely to introduce side effects because they monkey
+# patch toolkit. For now we're running the in-process because it makes
+# coverage more easier to retrieve
 
 
-def test_generate_doc_without_any_parameters():
+def test_without_any_parameters():
     """
-    Make sure we can generate documentation from inside a repo without any arguments.
+    Make sure we can generate documentation from inside a repository
+    without any arguments.
     """
     cwd = os.getcwd()
+    # Switch current folder to the framework do we can test both the core and
+    # framework detection code path.
     os.chdir(TK_FRAMEWORK_ROOT)
     try:
         assert tk_docs_preview.main(["tk_docs_preview", "--build-only"]) == 0
@@ -40,7 +44,7 @@ def test_generate_doc_without_any_parameters():
 
 
 # Disabling this test as the doc for the Python API is currently broken!
-def _test_generate_doc_with_python_api():
+def _test_with_python_api():
     """
     Make sure we can generate documentation for a non toolkit repo.
     """
@@ -52,7 +56,7 @@ def _test_generate_doc_with_python_api():
     )
 
 
-def test_generate_doc_with_tk_core():
+def test_with_tk_core():
     """
     Make sure we can generate documentation for tk-core
     """
@@ -64,7 +68,7 @@ def test_generate_doc_with_tk_core():
     )
 
 
-def test_generate_doc_with_tk_framework_shotgunutils():
+def test_with_tk_framework_shotgunutils():
     """
     Make sure we can generate documentation for a bundle that uses tk-core
     """
@@ -81,9 +85,9 @@ def test_generate_doc_with_tk_framework_shotgunutils():
     )
 
 
-def test_generate_doc_with_repo_without_doc():
+def test_with_repo_without_doc():
     """
-    Make sure we can generate documentation for a bundle that uses tk-core
+    Make sure the doc generation tool exits gracefully when there is no docs folder.
     """
     assert (
         tk_docs_preview.main(
@@ -98,9 +102,10 @@ def test_generate_doc_with_repo_without_doc():
     )
 
 
-def test_generate_doc_with_unknown_folder():
+def test_with_unknown_folder():
     """
-    Make sure we can generate documentation for a bundle that uses tk-core
+    Make sure the doc generation tool exits gracefully when we're not in a Toolkit
+    repo
     """
     assert (
         tk_docs_preview.main(
