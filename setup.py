192825018e9bda698aa520cc3a3c4bc3936ea5ed
Jean-François Boismenu <jfboismenu@users.noreply.github.com>
2020-09-29 13:58:29 -0400

    Pinning ordereddict  (#20)
    The developer of ruamel.ordereddict is not providing pre-built wheels for Python 2.7 anymore, or at the very least is not concerned by updating 2.7 wheels quickly, so we're locking down the version, as 0.4.15 does not have binary wheels at the moment.

--- setup.py
+++ setup.py
@@ -96,7 +96,8 @@ setup(
         "six==1.14.0",
     ]
     # Other tools used by devs that are useful to have.
-    + (["pre-commit", "ruamel.yaml"] if is_python_27_or_greater else []),
+    + (["pre-commit", "ruamel.yaml"] if is_python_27_or_greater else [])
+    + (["ruamel.ordereddict==0.4.14;sys_platform=='win32'"] if is_python_27 else []),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Framework :: Pytest",
