f9e62ac6c44ca3a01691fbfeebaba24dba425a34
mathurf <francis.mathurin@autodesk.com>
2021-01-12 15:48:38 -0500

    replacing os.environ with monkeypatch
    Using monkeypatch to avoid modifying the environment that can affect other tests.

--- tests/test_unique_name.py
+++ tests/test_unique_name.py
@@ -9,24 +9,23 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-import os
 from tk_toolchain.testing import create_unique_name
 
 
-def test_with_env_var():
+def test_with_env_var(monkeypatch):
     """
-    Ensure the env var is added to the name
+    Ensure the env var is added to the name.
+    Using monkeypatch to avoid modifying the environment that can affect other tests.
     """
-    os.environ["SHOTGUN_TEST_ENTITY_SUFFIX"] = "Potatoe"
+    monkeypatch.setenv("SHOTGUN_TEST_ENTITY_SUFFIX", "Potatoe")
     project_name = create_unique_name("Test-")
     assert project_name == "Test-Potatoe"
 
 
-def test_without_env_var():
+def test_without_env_var(monkeypatch):
     """
     Ensure we are only getting the name
     """
-    if "SHOTGUN_TEST_ENTITY_SUFFIX" in os.environ:
-        del os.environ["SHOTGUN_TEST_ENTITY_SUFFIX"]
+    monkeypatch.delenv("SHOTGUN_TEST_ENTITY_SUFFIX", raising=False)
     project_name = create_unique_name("Test")
     assert project_name == "Test"
