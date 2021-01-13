8cf75763a904dec1a10bec3d3cff5c8d23e3369d
mathurf <francis.mathurin@autodesk.com>
2021-01-13 08:20:48 -0500

    Adding a new method to create a unique project name. (#24)
    Ading a new method to create a unique project name if the env var SHOTGUN_TEST_ENTITY_SUFFIX is set. It takes a single parameter, the project name and then adds the content of the env var.

---
+++ tests/test_unique_name.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+from tk_toolchain.testing import create_unique_name
+
+
+def test_with_env_var(monkeypatch):
+    """
+    Ensure the env var is added to the name.
+    Using monkeypatch to avoid modifying the environment that can affect other tests.
+    """
+    monkeypatch.setenv("SHOTGUN_TEST_ENTITY_SUFFIX", "Potatoe")
+    project_name = create_unique_name("Test")
+    assert project_name == "Test - Potatoe"
+
+
+def test_without_env_var(monkeypatch):
+    """
+    Ensure we are only getting the name
+    """
+    monkeypatch.delenv("SHOTGUN_TEST_ENTITY_SUFFIX", raising=False)
+    project_name = create_unique_name("Test")
+    assert project_name == "Test"
