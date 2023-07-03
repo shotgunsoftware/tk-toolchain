06ae523c93d0ef33e51ea39318b7f02110e98a1b
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2023-07-03 14:27:06 -0500

    Revert on test_update_config

--- tests/test_tk_config_update.py
+++ tests/test_tk_config_update.py
@@ -14,7 +14,7 @@ import subprocess
 import six
 
 import pytest
-from ruamel.yaml import YAML
+from ruamel import yaml
 
 from tk_toolchain.cmd_line_tools import tk_config_update
 
@@ -129,12 +129,10 @@ def test_update_config(
     assert updated_files == [os.path.join(test_config, modified_file)]
 
     with open(os.path.join(cloned_config, modified_file), "rt") as fh:
-        yaml = YAML(typ="unsafe", pure=True)
-        expected_cfg = yaml.load(fh)
+        expected_cfg = yaml.load(fh, Loader=yaml.Loader)
 
     with open(os.path.join(test_config, modified_file), "rt") as fh:
-        yaml = YAML(typ="unsafe", pure=True)
-        test_config = yaml.load(fh)
+        test_config = yaml.load(fh, Loader=yaml.Loader)
 
     value = expected_cfg
     for key in path_to_descriptor:
