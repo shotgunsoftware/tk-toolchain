766b66dcf793180d08111351d2f6e90fe40f9525
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2023-07-03 12:12:10 -0500

    Bumps pytest-cov to remove warnings

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ is_python_27 = python_version == (2, 7)
 
 pytest_version = "pytest==4.6.6" if is_python_27_or_greater else "pytest<3.3"
 pytest_cov_version = (
-    "pytest-cov==2.6.1" if is_python_27_or_greater else "pytest-cov==2.5.1"
+    "pytest-cov==4.0.0" if is_python_27_or_greater else "pytest-cov==2.5.1"
 )
 pyyaml_version = "PyYAML" if is_python_27_or_greater else "PyYAML==3.11"
 
