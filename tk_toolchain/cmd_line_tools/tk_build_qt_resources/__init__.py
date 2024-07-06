d1bed3d21d2f72a19596a8459d54f9d67b8e12a0
Eduardo Chauca <eduardo.chauca@autodesk.com>
2024-07-06 03:55:49 -0500

    new name for py files are accepted

--- tk_toolchain/cmd_line_tools/tk_build_qt_resources/__init__.py
+++ tk_toolchain/cmd_line_tools/tk_build_qt_resources/__init__.py
@@ -14,7 +14,7 @@ Toolkit Build Qt resources
 Compile Qt interface and resource files with a specified PySide compiler.
 
 Usage:
-    tk-build-qt-resources (-p <pyenv> | [-u <uic>] [-r <rcc>]) -q <qtuipath> -py <pybuiltpath> -uf <uifiles>... -rf <resfiles>... [-i <importtext>]
+    tk-build-qt-resources (-p <pyenv> | [-u <uic>] [-r <rcc>]) -q <qtuipath> -py <pybuiltpath> -uf <uifiles>... [-ufn <uifilenames>...] -rf <resfiles>... [-rfn <resfilenames>...] [-i <importtext>]
 
 Options:
     -u --uic        The PySide uic compiler.
@@ -23,13 +23,17 @@ Options:
     -q --qtuipath   The path with Qt .ui files.
     -py --pybuiltpath The path to output all built .py files.
     -uf --uifiles   The Qt .ui files to compile.
+    -ufn --uifilenames   Specific Qt .ui file names to compile.
     -rf --resfiles  The Qt .qrc resource files to compile.
+    -rfn --resfilenames  Specific Qt .qrc file names to compile.
     -i --importtext The import text to replace (default is "tank.platform.qt").
 
 Examples:
     tkbuild-qt-resources -u /path/to/pyside2-uic -r /path/to/pyside2-rcc -q /path/to/qt/ui/files -py /path/to/output/py/files -uf file1 file2 -rf resource1 resource2 -i custom.import.path
 
     tkbuild-qt-resources -p /path/to/python/env -q /path/to/qt/ui/files -py /path/to/output/py/files -uf file1 file2 -rf resource1 resource2 -i custom.import.path
+
+    tkbuild-qt-resources -p /path/to/python/env -q /path/to/qt/ui/files -py /path/to/output/py/files -uf file1 file2 -ufn ui_file1 ui_file2 -rf resource1 resource2 -i custom.import.path
 """
 
 import argparse
@@ -73,19 +77,19 @@ def build_qt(compiler, py_filename, py_built_path, import_text):
     open(output_path, "w").write(content)
 
 
-def build_ui(compiler, qt_ui_path, py_built_path, import_text, filename):
+def build_ui(compiler, qt_ui_path, py_built_path, import_text, filename, py_filename=None):
     return {
         "compiler": f"{compiler} -g python --from-imports {qt_ui_path}/{filename}.ui",
-        "py_filename": filename,
+        "py_filename": py_filename or filename,
         "py_built_path": py_built_path,
         "import_text": import_text,
     }
 
 
-def build_res(compiler, qt_ui_path, py_built_path, import_text, filename):
+def build_res(compiler, qt_ui_path, py_built_path, import_text, filename, py_filename=None):
     return {
         "compiler": f"{compiler} -g python {qt_ui_path}/{filename}.qrc",
-        "py_filename": f"{filename}_rc",
+        "py_filename": f"{py_filename or filename}_rc",
         "py_built_path": py_built_path,
         "import_text": import_text,
     }
@@ -113,14 +117,26 @@ def main():
         "--uifiles",
         nargs="+",
         required=True,
-        help="The path with resources Qt .ui files",
+        help="The Qt .ui files to compile.",
+    )
+    parser.add_argument(
+        "-ufn",
+        "--uifilenames",
+        nargs="+",
+        help="Specific Qt .ui file names to compile.",
     )
     parser.add_argument(
         "-rf",
         "--resfiles",
         nargs="+",
         required=True,
-        help="The path to output all built .py files to",
+        help="The Qt .qrc resource files to compile.",
+    )
+    parser.add_argument(
+        "-rfn",
+        "--resfilenames",
+        nargs="+",
+        help="Specific Qt .qrc file names to compile.",
     )
     parser.add_argument(
         "-i",
@@ -156,15 +172,21 @@ def main():
     }
 
     print("Building user interfaces...")
-    for ui_file in args.uifiles:
-        build_params["compiler"] = args.uic
-        build_params["filename"] = ui_file
-        build_qt_params = build_ui(**build_params)
+    for index, ui_file in enumerate(args.uifiles):
+        build_params_ui_files = build_params.copy()
+        build_params_ui_files["compiler"] = args.uic
+        build_params_ui_files["filename"] = ui_file
+        if args.uifilenames:
+            build_params_ui_files["py_filename"] = args.uifilenames[index]
+        build_qt_params = build_ui(**build_params_ui_files)
         build_qt(**build_qt_params)
 
     print("Building resources...")
-    for res_file in args.resfiles:
-        build_params["compiler"] = args.rcc
-        build_params["filename"] = res_file
-        build_qt_params = build_res(**build_params)
+    for index, res_file in enumerate(args.resfiles):
+        build_params_res_files = build_params.copy()
+        build_params_res_files["compiler"] = args.rcc
+        build_params_res_files["filename"] = res_file
+        if args.resfilenames:
+            build_params_res_files["py_filename"] = args.resfilenames[index]
+        build_qt_params = build_res(**build_params_res_files)
         build_qt(**build_qt_params)
