d388dcad58a5ba901302d94aff7089fe5099b3dc
Julien Langlois <julien.langlois@autodesk.com>
2026-09-17 12:02:16 -0700

    SG-45250 Fix pytest_ignore_collect for pytest 9 (path -> collection_path)

--- pytest_tank_test/__init__.py
+++ pytest_tank_test/__init__.py
@@ -184,11 +184,11 @@ def _ensure_dependencies(repo):
                 )
 
 
-def pytest_ignore_collect(path, config):
+def pytest_ignore_collect(collection_path, config):
     """
     Ignore unit tests for third parties found inside tk-core and any Python
     source file inside tests/fixtures.
     """
-    return os.path.join("tests", "python", "third_party") in str(path) or os.path.join(
-        "tests", "fixtures"
-    ) in str(path)
+    return os.path.join("tests", "python", "third_party") in str(
+        collection_path
+    ) or os.path.join("tests", "fixtures") in str(collection_path)
